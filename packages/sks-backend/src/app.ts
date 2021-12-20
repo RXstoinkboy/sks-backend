d6ced30ece1b4ab8638e56e5468eda564223e0cf
rxstoinkboy <erykludwin@gmail.com>
2021-12-21 00:41:36 +0100

    feat: added first mutation in gql but it still needs some tweaks to properly return a value

--- packages/sks-backend/src/app.ts
+++ packages/sks-backend/src/app.ts
@@ -1,24 +1,122 @@
 import { ApolloServer, gql } from 'apollo-server-express';
+import { DataSource } from 'apollo-datasource';
 import { PORT } from './constants/global';
 import { db } from './config/db';
 import { errorHandler } from './middleware/errorHandler.middleware';
+// import { merge } from 'lodash';
 import { notFoundHandler } from './middleware/notFound.middleware';
 import router from './routes';
 import { setupAppConfig } from './config/app.config';
 
 import * as express from 'express';
 import * as http from 'http';
+import * as mongoose from 'mongoose';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 
-const typeDefs = gql`
-   type Query {
-      hello: String!
+const baseTypeDefs = gql`
+   type Query
+   type Mutation
+`;
+
+const userTypeDefs = gql`
+   type User {
+      id: ID!
+      email: String!
+      password: String!
+      expenses: [Expense]!
+   }
+
+   input CreateUserDTO {
+      email: String!
+      password: String!
+   }
+
+   input UpdateUserDTO {
+      email: String
+      password: String
+   }
+
+   extend type Query {
+      user(id: ID!): User
+      users: [User]!
+   }
+
+   extend type Mutation {
+      createUser(newUser: UpdateUserDTO): User
+      updateUser(userData: UpdateUserDTO): User
+      removeUser(userId: ID!): String
    }
 `;
-const resolvers = {
-   Query: {
-      hello: () => 'hello',
+
+// TODO: 'expenses' Query pewnie jeszcze będzie do wywalenia, bo raczej nie ma sensu pobierać wszystkie wydatki
+// TODO: trzeba to będzie jeszcze porządnie zmodularyzować
+const expenseTypeDefs = gql`
+   type Expense {
+      id: ID!
+      value: Int!
+      user: User!
+   }
+
+   input CreateExpenseDTO {
+      value: Int!
+   }
+
+   input UpdateExpenseDTO {
+      value: Int!
+   }
+
+   extend type Query {
+      expense(id: ID!): Expense
+      expenses: [Expense]!
+   }
+
+   extend type Mutation {
+      createExpense(newExpense: CreateExpenseDTO): Expense
+      updateExpense(expenseData: UpdateExpenseDTO): Expense
+      removeExpense(expenseId: ID!): Boolean
+   }
+`;
+
+const typeDefs = [baseTypeDefs, userTypeDefs, expenseTypeDefs];
+
+interface User extends mongoose.Document {
+   email: string;
+   password: string;
+   expenses: any[]; // TODO: replace with Expense model
+}
+
+const UserSchema = new mongoose.Schema<User>({
+   email: {
+      required: true,
+      type: String,
    },
+   expenses: {
+      required: true,
+      type: [],
+   },
+   password: {
+      required: true,
+      type: String,
+   },
+});
+
+const UserModel = mongoose.model<User>('User', UserSchema);
+class UserAPI extends DataSource {
+   createUser = async ({ newUser }) => {
+      const newUserEntity = new UserModel(newUser);
+      // TODO: poprawić jeszcze to żeby zwracało wynik, bo aktualnie user się tworzy ale nie da się zwrócić stworzonego usera
+      const createdUser = await newUserEntity.save();
+      return createdUser;
+   };
+}
+
+const resolvers = {
+   Mutation: {
+      createUser: (_, args, { dataSources }) => {
+         dataSources.UserAPI.createUser(args);
+      },
+   },
+   Query: {},
 };
 
 const startServer = async (typeDefs, resolvers) => {
@@ -26,6 +124,9 @@ const startServer = async (typeDefs, resolvers) => {
    const httpServer = http.createServer(app);
 
    const server = new ApolloServer({
+      dataSources: () => ({
+         UserAPI: new UserAPI(),
+      }),
       plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
       resolvers,
       typeDefs,
